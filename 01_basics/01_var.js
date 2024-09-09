606ce9bd2ced03ae48689a0e3a6fceebaea545e7
Ubaidullah <90694531+Ubaidullah789@users.noreply.github.com>
2024-09-09 14:42:04 +0000

    bd

---
+++ 01_basics/01_var.js
@@ -0,0 +1,8 @@
+const AccId = 39937951931;
+let AccName = "Obaidullah"
+var AccCity = "Srinagar"  // nahi use karna chahiye
+AccPassword = "Password..."
+AccCity = "Gurugram"
+let NewCity
+
+console.table([AccId, AccCity, AccName, AccPassword, NewCity])

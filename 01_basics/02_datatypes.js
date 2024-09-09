47711aec4830c1368c867ec431d277712f404bf8
Ubaidullah <90694531+Ubaidullah789@users.noreply.github.com>
2024-09-09 11:46:23 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all JScode as newer version
+// atert(3 + 3) we are using nodejs not browser
+console.log(3 + 3);
+let name = "Obaid" // string
+let age = 23 // number
+let isLoggedin = false
+
+// number -------->>>  2 to the power of 53   (range)
+// bigint -------->>>  bada bada number
+// string -------->>> ''  or "" 
+// boolean ------->>> True or False
+// Null ---------->>> standalone value  representation of empty           ------------>>>Null is an object!!!!
+// Undefined------>>> Value abi assigned nahi hai
+// Symbol -------->>> For Uniqueness
+
+
+// object
+
+console.log(typeof undefined);

d9a267c1f747597d96a21f76d63d1d3677a66fac
Ubaidullah <90694531+Ubaidullah789@users.noreply.github.com>
2024-09-09 12:40:21 +0000

    strconvDone

--- 01_basics/03_datatypeConversions.js
+++ 01_basics/03_datatypeConversions.js
@@ -1,10 +1,10 @@
 "use strict"
 let score = "hitest"
-console.log(typeof score);
-console.log(typeof (score));
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);   // NaN is Not a Number  
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);   // NaN is Not a Number  
 
 
 // "33" -- 33
@@ -14,15 +14,42 @@ console.log(valueInNumber);   // NaN is Not a Number
 
 let isLoggedIn = undefined
 let BooleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(isLoggedIn);
-console.log(BooleanIsLoggedIn);
+// console.log(isLoggedIn);
+// console.log(BooleanIsLoggedIn);
 
 
 let aNumber = 33
 let StrNum = String(aNumber)
-console.log(StrNum);
-console.log(typeof StrNum);
+// console.log(StrNum);
+// console.log(typeof StrNum);
 
 
 
+// **********************Operations*****************************
+
+let value =3 
+let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2= " obaid"
+let str3 = str1 + str2
+// console.log(str3);
+let new1 = ""
+// console.log(+new1);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter =100
+++gameCounter
+console.log(gameCounter);
 

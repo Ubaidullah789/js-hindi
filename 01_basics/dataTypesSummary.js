72b2d7a8759ebdbfeb87cc96c1ffba0144b9bb6b
Ubaidullah <90694531+Ubaidullah789@users.noreply.github.com>
2024-09-09 14:41:53 +0000

    datatypesummart

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,39 @@
+// Premitive datatypes and non premitive datatypes
+// Call by Value and Call by Refrence
+//  Premitive are call by value
+
+// & types of primitive DataTypes:
+// 1. String
+// 2. NUmber
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol
+// 7. BigInt
+
+// Non Primitive/ Refrence Type
+// Arrays, Objects, Functions
+
+let id = Symbol('123')
+let zid = Symbol('123')
+// console.log(id);
+// console.log(zid);
+// console.log(id === zid);
+
+const BigNum = 1234566789n
+// console.log(typeof BigNum);
+// console.log(BigNum);
+ const heros = ['spiderman', 'batman', 'ironman']
+
+ let myObj = {
+    name:"Obaid",
+    age:23
+ }
+
+ const myFunction = function(){
+    console.log("hi Zafar");
+    
+ }
+ console.log(typeof heros);
+ 
+
